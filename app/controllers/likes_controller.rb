088152af4df844864e2924b3ee6b122300f36c61
RealTeeCee <nguyenngocnguyen.rtc@gmail.com>
2023-04-07 14:39:41 +0700

    Use form_with Helper

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
     skip_before_action :verify_authenticity_token
 
-    def update
+    def create
         Like.create(likeable_id: params["likeable_id"], likeable_type: 'Post')
         redirect_to post_url(params["likeable_id"])
     end
 
-    def remove
+    def destroy
         like = Like.where(likeable_id: params["likeable_id"], likeable_type: 'Post').first
         if like
             like.destroy 
